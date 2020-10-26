9a6fa02bebcf546e0881efff4ce9cbc006b415cd
cesaregarciaudem <72897303+cesaregarciaudem@users.noreply.github.com>
2020-10-26 13:30:15 -0600

    Add files via upload

---
+++ decode_token.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 13:17:10 2020
+
+@author: cesar
+"""
+import pyjwt as jwt
+
+@staticmethod
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
